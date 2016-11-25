06b459f7f899908cb77f9f925fb0666d2739df19
François Massot <francois.massot@gmail.com>
2016-11-25 22:23:33 +0100

    first commit ! (and probably last one :))

---
+++ crawl_data.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import requests
+
+data = requests.get("https://en-marche.fr/api/stats").json()
+data['date'] = datetime.datetime.today().replace(microsecond=0).isoformat()
+
+print('{date},{committeeCount},{eventCount},{userCount}'.format(**data))
