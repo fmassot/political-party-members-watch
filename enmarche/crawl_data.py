33c950545a7269a8e7601755f99c79b2846c9493
François Massot <francois.massot@gmail.com>
2017-03-02 10:27:00 +0100

    Update crawl_data.py

--- enmarche/crawl_data.py
+++ enmarche/crawl_data.py
@@ -3,7 +3,7 @@
 import datetime
 import requests
 
-data = requests.get("https://adherents.en-marche.fr/api/stats").json()
+data = requests.get("https://en-marche.fr/api/stats").json()
 data['date'] = datetime.datetime.today().replace(microsecond=0).isoformat()
 
 print('{date},Macron,{userCount},{committeeCount},{eventCount}'.format(**data))
